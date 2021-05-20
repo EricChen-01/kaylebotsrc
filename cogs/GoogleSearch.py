f3be6cf69ad894dd3ef69dc80655f0014cb86492
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-05-19 21:32:39 -0400

    asd

--- cogs/GoogleSearch.py
+++ cogs/GoogleSearch.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-from google_images_download import google_images_download 
 
 
 class GoogleSearch(commands.Cog):
@@ -10,31 +9,6 @@ class GoogleSearch(commands.Cog):
   #commands
   @commands.command()
   async def googleImage(self, ctx, *message):
-    downloadimages(query=message)
   
 def setup(client):
   client.add_cog(GoogleSearch(client))
-
-def downloadimages(query):
-    response = google_images_download.googleimagesdownload()
-    arguments = {"keywords": query,
-                 "format": "jpg",
-                 "limit":1,
-                 "print_urls":True,
-                 "size": "medium",
-                 "aspect_ratio":"panoramic"}
-    try:
-        response.download(arguments)
-        print()
-
-    except FileNotFoundError: 
-        arguments = {"keywords": query,
-                     "format": "jpg",
-                     "limit":1,
-                     "print_urls":True, 
-                     "size": "medium"}
-
-        try:
-            response.download(arguments) 
-        except:
-            pass
