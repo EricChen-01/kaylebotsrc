dba9eea0b4fff444509bff7e1a9df4460332272d
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-02 14:56:26 -0400

    asd

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def on_command_error(ctx, error):
   elif isinstance(error, commands.MissingRequiredArgument):
     await ctx.send(f"Sorry {displayName}! Missing required arguments!")
   elif isinstance(error, commands.CommandOnCooldown):
-    await ctx.send(f"Sorry {displayName}! This command is on cooldown. Please try again in {error.retry_after} seconds")
+    await ctx.send(f"Sorry {displayName}! This command is on cooldown. Please try again in {round(error.retry_after, 2)} seconds")
   else:
     await ctx.send(f"Sory {displayName}! I don't know this command!")
   pass
