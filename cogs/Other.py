12e38c1c7b9b8fac8700d419f44af44148391684
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-02 14:05:25 -0400

    asd

--- cogs/Other.py
+++ cogs/Other.py
@@ -14,13 +14,13 @@ class Other(commands.Cog):
     @commands.command()
     @commands.cooldown(1,3600, commands.BucketType.default)
     async def weather(self,ctx, *,city="New York City"):
-        response = requests.get(f"api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('TOKEN')}")
-        currently = response.json()["weather"]["main"]
-        description = response.json()["weather"]["description"]
-        temperature = response.json()["weather"]["temp"]
+        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('TOKEN')}")
+        currently = response.json()["weather"][0]["main"]
+        description = response.json()["weather"][0]["description"]
+        temperature = response.json()["main"]["temp"]
         fahrenheit = ((temperature - 273.15) * 9) / 5
         celsius = (fahrenheit - 32) * (5/9)
-        await ctx.send(f'{city}: currently:{currently}\ndescription:{description}\nTemperature:{fahrenheit}°F    {celsius}°C    {temperature}°K')
+        await ctx.send(f'{city}: currently:{currently}\ndescription:{description}\nTemperature:{round(fahrenheit,2)}°F    {round(celsius,2)}°C    {round(temperature,2)}°K')
         
 def setup(client):
   client.add_cog(Other(client))
