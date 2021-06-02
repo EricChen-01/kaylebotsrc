bee9f60277c7455731f7698ac3c753786cf4ecb0
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-02 14:51:05 -0400

    asd

--- cogs/Other.py
+++ cogs/Other.py
@@ -13,7 +13,7 @@ class Other(commands.Cog):
     #commands
     @commands.command()
     @commands.cooldown(1,3600, commands.BucketType.default)
-    async def weather(self,ctx, city="New York City"):
+    async def weather(self,ctx, *,city="New York City"):
         response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv('WeatherAPI')}")
         currently = response.json()["weather"][0]["main"]
         description = response.json()["weather"][0]["description"]
