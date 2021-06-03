4a3635e1d0a44a60ff27f02bc696759cd78d3522
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-03 18:00:14 -0400

    asd

--- cogs/Other.py
+++ cogs/Other.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 import discord
 import requests
+import datetime
 from discord.ext import commands
 
 
@@ -20,7 +21,18 @@ class Other(commands.Cog):
         temperature = response.json()["main"]["temp"]
         fahrenheit = (((temperature - 273.15) * 9) / 5) + 32
         celsius = (fahrenheit - 32) * (5/9)
-        await ctx.send(f'{city}: currently:{currently}\ndescription:{description}\nTemperature:{round(fahrenheit,2)}°F    {round(celsius,2)}°C    {round(temperature,2)}°K')
+        weatherIcon = response.json()['weather'][0]['icon']
+        
+        embed = discord.Embed(title=f"***Weather Report: {city}***",color=0x14749F)
+        embed.add_field(name='***Currently***', value=f'{currently}', inline=True)
+        embed.add_field(name='***Description***', value=f'{description}', inline=True)
+        embed.add_field(name='***Temperature***', value=f'{round(fahrenheit,2)}°F \n{round(celsius,2)}°C \n{round(temperature,2)}°K', inline=True)
+
+        embed.set_thumbnail(url=f'http://openweathermap.org/img/wn/{weatherIcon}@2x.png')
+        embed.set_author(name=f'{ctx.author.display_name}', icon_url=f'{ctx.author.avatar_url}')
+        embed.set_footer(text=f"{ctx.author.guild}", icon_url=f"{ctx.author.guild.icon_url}")
+        embed.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed=embed)
         
 def setup(client):
   client.add_cog(Other(client))
