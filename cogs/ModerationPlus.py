88f73d72f631c7cf1278e88f22e36a03511b440d
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-04 13:39:48 -0400

    asd

--- cogs/ModerationPlus.py
+++ cogs/ModerationPlus.py
@@ -348,11 +348,11 @@ class ModerationPlus(commands.Cog):
 
   @commands.group(invoke_without_command=True)
   async def experimental(self,ctx):
-    em = discord.Embed(title=f"***Server Setup For {ctx.guild.name}***",color=0x14749F)
-    em.add_field(name='***Join/Leave Channel***', value=f'Please reply with channel id.', inline=True)
-    em.set_footer(text=f"{ctx.author.guild}", icon_url=f"{ctx.author.guild.icon_url}")
-    em.timestamp = datetime.datetime.utcnow()
-    sent = await ctx.send(embed=em)
+    setup = discord.Embed(title=f"***Server Setup For {ctx.guild.name}***",color=0x14749F)
+    setup.add_field(name='***Join/Leave Channel***', value=f'Please reply with channel id.', inline=True)
+    setup.set_footer(text=f"{ctx.author.guild}", icon_url=f"{ctx.author.guild.icon_url}")
+    setup.timestamp = datetime.datetime.utcnow()
+    sent = await ctx.send(embed=setup)
 
     try:
       reply = await self.client.wait_for(
@@ -363,7 +363,38 @@ class ModerationPlus(commands.Cog):
       if reply: 
         await sent.delete()
         await reply.delete()
-        await ctx.send(reply.content)
+
+        setup.clear_fields()
+        setup.add_field(name='***Join Message***', value=f'Please reply with a message.', inline=True)
+
+        sent = await ctx.send(embed=setup)
+
+        reply = await self.client.wait_for(
+        "message",
+        timeout=10,
+        check = lambda message: message.author == ctx.author and message.channel == ctx.channel
+        ) 
+        if reply:
+          await sent.delete()
+          await reply.delete()
+
+          setup.clear_fields()
+          setup.add_field(name='***Leave Message***', value=f'Please reply with a message.', inline=True)
+
+          sent = await ctx.send(embed=setup)
+
+          reply = await self.client.wait_for(
+            "message",
+            timeout=10,
+            check = lambda message: message.author == ctx.author and message.channel == ctx.channel
+          ) 
+          if reply:
+            await sent.delete()
+            await reply.delete()
+
+            await ctx.send("complete")
+
+
     except asyncio.TimeoutError:
       await sent.delete()
       await ctx.send('Setup timedout.', delete_after=10)
