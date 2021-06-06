80dca49687cd5719f1f7ef3a44d7202a4993e0d2
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-05 21:49:37 -0400

    asd

--- main.py
+++ main.py
@@ -40,9 +40,7 @@ async def on_command_error(ctx, error):
     await ctx.send(f"Sorry {displayName}! Missing required arguments!")
   elif isinstance(error, commands.CommandOnCooldown):
     await ctx.send(f"Sorry {displayName}! This command is on cooldown. Please try again in {round(error.retry_after, 2)} seconds")
-  else:
-    await ctx.send(f"Sory {displayName}! I don't know this command!")
-  pass
+  
 
 
 #cog loaders
