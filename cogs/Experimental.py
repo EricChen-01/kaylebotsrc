013eee6979ca0c352cde3089917f6f67d0db5d08
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-05 21:44:24 -0400

    asd

--- cogs/Experimental.py
+++ cogs/Experimental.py
@@ -37,14 +37,14 @@ async def serverSet(self,ctx):
     em.add_field(name='NOTE: replying with "NONE" will skips steps 2 and 3.', inline=False)
     sent = await ctx.send(embed=em)
     response = await respond(self=self,ctx=ctx)
-    await sent.add_reaction('\U00000031')
+    #await sent.add_reaction('\U00000031')
     if response != "NONE":
         channel = response
         channelComplete = True
 
     #join message
     await clearEmbed(self, ctx, em)
-    await sent.add_reaction('\U00000032')
+    #await sent.add_reaction('\U00000032')
     if channelComplete:
         em.add_field(name='***Join Message: Step 2/4***', value='reply with a join message. Reply with "NONE" to skip this step.', inline=True)
         await sent.edit(embed=em)
@@ -54,7 +54,7 @@ async def serverSet(self,ctx):
 
     #leave message
     await clearEmbed(self,ctx,em)
-    await sent.add_reaction('\U00000033')
+    #await sent.add_reaction('\U00000033')
     if channelComplete:
         em.add_field(name='***Leave Message: Step 3/4***', value='reply with a Leave Message. Reply with "NONE" to skip this step.', inline=True)
         await sent.edit(embed=em)
@@ -65,7 +65,7 @@ async def serverSet(self,ctx):
 
     #audit log
     await clearEmbed(self,ctx,em)
-    await sent.add_reaction('\U00000034')
+    #await sent.add_reaction('\U00000034')
     em.add_field(name='***Audit Log: Step 4/4***', value='reply with a channel id. Reply with "NONE" to skip this step.', inline=True)
     await sent.edit(embed=em)
     response = await respond(self=self,ctx=ctx)
