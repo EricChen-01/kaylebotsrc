e4844057931cbc8155810ac8b079dc5c6830af0b
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-06-06 20:24:16 -0400

    asd

--- cogs/Experimental.py
+++ cogs/Experimental.py
@@ -23,7 +23,7 @@ class Experimental(commands.Cog):
     response = await respond(self,ctx)
     print(response)
     if response.startswith('<#'):
-        channelName = response[2:1]
+        channelName = response[2:-1]
         channel =  self.client.get_channel(name=channelName)
     elif response.isdecimal():
         channel =  self.client.get_channel(id=int(response))
